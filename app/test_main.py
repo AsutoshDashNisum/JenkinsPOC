65aa2eef8488dd9a0a8d8363060afc3fedbd5473
adash <adash@nisum.com>
2026-03-05 16:01:05 +0530

    Fix PEP 8 style issues to pass Lint stage

--- app/test_main.py
+++ app/test_main.py
@@ -1,10 +1,11 @@
-
 import unittest
 from main import hello
+
 
 class TestMain(unittest.TestCase):
     def test_hello(self):
         self.assertEqual(hello(), "Hello, Jenkins CI/CD!")
+
 
 if __name__ == "__main__":
     unittest.main()
